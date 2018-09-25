cbee10c0d9b170d4cb510a6ccbdc4f8b53c8cdf7
Ida Egendal <idaegendal@Idas-MacBook-Air.local>
2018-09-25 13:22:34 +0200

    Created LLN

--- R/LLN.R
+++ R/LLN.R
@@ -1,15 +1,15 @@
-#' CLT function
+#' LLN function
 #'
 #'@examples
 #'y<-nsteps22(0,1000,0.8,rnorm(1000))
-#'CLT(y)
+#'LLn(y)
 #'@export
-CLT<-function(y){
+
+LLN<-function(y){
   v<-c()
   for(i in 1:length(y)){
-    v[i]=(1/sqrt(i))*sum(y[1:i])
+    v[i]=(1/i)*sum(y[1:i])
   }
   plot(v,type="l")
+  abline(h=mean(y), col="cyan2")
 }
-
-
