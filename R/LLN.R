d2b3b517311f7ff68659e018b5acc6bc8d024782
KatrineH96 <katrine.holde@outlook.com>
2018-10-02 12:36:19 +0200

    Changed LLN so it takes a matrix as the parameter instead of a vector.

--- R/LLN.R
+++ R/LLN.R
@@ -3,19 +3,31 @@
 #' A function that checks, if there is a Law of Large Numbers
 #' Calculates form the first to the the n'th step of sum(Yi)/n, to determine if there is a Law of Large Numbers.
 #'
-#'@param y vector of random variables.
+#'@param x a matrix where every column is a vector of random variables with the same parametervalues.
 #'
-#'@return A plot through the values \eqn{sum_{i=1}^{k}y_j/k} for each k from 1 to n, in addition to a line through the mean of y.
+#'@return A plot through the values \eqn{sum_{i=1}^{k}y_j/k} for each k from 1 to n.
 #'
-#'@details If a given output of this function resembles that it converges towards the line through the mean of y, it would seem that there is a Law of Large Numbers.
+#'@details If a given output of this function resembles that it converges towards the same value, it would seem that there is a Law of Large Numbers.
 #'
 #'@examples
-#'y<-simAR1(0,1000,0.8,rnorm(1000))
-#'LLN(y)
+#'x<-replicate(10, simAR1(0,1000,0.8,rnorm(1000)))
+#'LLN(x)
 #'@export
 
-LLN<-function(y){
+LLN1<-function(y){
   v <- cumsum(y)/(1:length(y))
-  plot(v,type="l")
-  abline(h=mean(y), col="cyan2")
+  v
 }
+
+LLN <- function(x){
+  n <- ncol(x)
+  c <- matrix(c(rep(0,length(x))), nrow=nrow(x), ncol=n)
+  for(i in 1:n){
+    c[,i]=LLN1(x[,i])
+  }
+  c
+  matplot(c, type = "l")
+}
+
+x <- replicate(10, simAR1(0, 5000, 0.8, rnorm(5000)))
+LLN(x)
