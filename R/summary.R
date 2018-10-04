8860b240d463c116664b735d140681ee28c49912
KatrineH96 <katrine.holde@outlook.com>
2018-10-04 21:22:02 +0200

    Made the description for the summary function

--- R/summary.R
+++ R/summary.R
@@ -1,5 +1,21 @@
+#' Object summary
+#'
+#'Gives an summary of a simulation.
+#'
+#'@param p a matrix of class "simulation". 
+#'
+#'@return It returns a summary. The summary contains the length of the individual simulation, the value of the
+#'parameter phi, the value of the initial value Y_0, and (...). 
+#'
+#'@details 
+#'
+#'@examples
+#'p <- rAR1(0, 300, 0.8, distfun = rnorm, 300)
+#'summary(p)
+#'  
+#'@export
 
 summary.simulation<-function(p){
-  cat(attr(p,"type"), "realisations of an AR(1) process with length","\n", attr(p,"steps"), ", intital value", attr(p,"initial_value"), 
-      ", phi=", attr(p,"phi"))
+  cat(attr(p,"type"), "realisations of an AR(1) process with length","\n", attr(p,"steps"), 
+      ", intital value", attr(p,"initial_value"), ", phi=", attr(p,"phi"))
 }
